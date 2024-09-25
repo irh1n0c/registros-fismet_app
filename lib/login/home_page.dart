400484cb7509338f29752642f029512cfd1b9917
irh1n0c <lhuaynasi@gmail.com>
2024-09-25 09:19:04 -0500

    create button form

--- lib/login/home_page.dart
+++ lib/login/home_page.dart
@@ -1,33 +1,48 @@
 import 'package:flutter/material.dart';
-import 'tmpt/login_text.dart';
+import 'package:fismet_formsapp/templates/button_form/button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    final userController = TextEditingController(); // Define el controlador
+    final userController = TextEditingController();
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Formulario ok pe',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            //Uso del template de boxes
-            TextFieldInpute(
+      body: Stack( 
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Formulario ok pe',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                // Uso del template de boxes
+                TextFieldInpute(
                   textEditingController: userController,
-                  hintText: "Ingrese código de registro.",
+                  hintText: "EL AMIRU ME LA PELA",
                   icon: Icons.article_rounded,
                 ),
-          ],
-        ),
+              ],
+            ),
+          ),
+          // Imagen en la esquina superior izquierda
+          Positioned(
+            top: 40, // Ajusta la posición vertical
+            left: 10, // Ajusta la posición horizontal
+            child: Image.asset(
+              'assets/icon/registros.png', 
+              width: 50, // Ancho de la imagen
+              height: 50, // Alto de la imagen
+            ),
+          ),
+          
+        ],
       ),
     );
   }
 }
+
--- lib/templates/button_form/button.dart
+++ lib/templates/button_form/button.dart
@@ -28,31 +28,31 @@ Widget build(BuildContext context) {
       decoration: InputDecoration(
         hintText: hintText,
         hintStyle: const TextStyle(
-          color: Color(0xFF1d277f),
+          color: Color(0xFF900909),
           fontWeight: FontWeight.w300,
           fontSize: 15,
           ),
         prefixIcon: Icon(
           icon,
-          color: const Color(0xFF1d277f)),
+          color: const Color(0xFF900909)),
         contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
         border: InputBorder.none,
         filled: true,
-        fillColor: const Color.fromARGB(217, 203, 202, 224),
+        fillColor: const Color.fromARGB(53, 231, 209, 209),
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide.none,
           borderRadius: BorderRadius.circular(20),
         ),
         
         focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(width: 0.5, color: Color(0xFF1d277f)),
+          borderSide: const BorderSide(width: 0.5, color: Color(0xFFB3B3B3)),
           borderRadius: BorderRadius.circular(20),
         ),
         suffixIcon: isPass
             ? IconButton(
                 icon: Icon(
                   obscureText ? Icons.visibility : Icons.visibility_off,
-                  color: const Color(0xFF1d277f),
+                  color: const Color(0xFF900909),
                 ),
                 onPressed: () {
                   if (onObscureTextChanged != null) {
